2b1a0444e56b463000be17658c763ffdc8e6255a
Noé Masse <s.masse.noe@gmail.com>
2021-03-27 13:46:41 +0100

    Checkout Meta

---
+++ Assets/Common/Scripts/Core/SceneLoader.cs
@@ -0,0 +1,80 @@
+﻿//-----------------------------------------------------------------
+// File:         SceneLoader.cs
+// Description:  Load and Unload Scenes
+// Module:       Unity Core
+// Author:       Noé Masse
+// Date:         17/03/2021
+//-----------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+
+namespace MonsterWorld.Unity
+{
+    public enum SceneLoadingResult
+    {
+        Succeeded,
+        Failed
+    }
+
+    public class SceneLoader
+    {
+        public class SceneLoadingOperation
+        {
+            public Action<SceneLoadingResult> Completed;
+        }
+
+        private Queue<AssetReferenceScene> _scenesToLoad;
+        private bool _isReady;
+        private SceneLoadingOperation _operation;
+
+        public bool IsReady => _isReady;
+
+        public SceneLoader()
+        {
+            _isReady = true;
+            _operation = new SceneLoadingOperation();
+        }
+
+        public SceneLoadingOperation LoadScenes(params AssetReferenceScene[] scenes)
+        {
+            if (scenes.Length == 0) return null;
+
+            _scenesToLoad = new Queue<AssetReferenceScene>(scenes);
+            _isReady = false;
+
+            _scenesToLoad.Dequeue().LoadSceneAsync(LoadSceneMode.Additive).Completed += ContinueSceneLoading;
+            return _operation;
+        }
+
+        private void ContinueSceneLoading(AsyncOperationHandle<SceneInstance> handle)
+        {
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                if (_scenesToLoad.Count > 0)
+                {
+                    _scenesToLoad.Dequeue().LoadSceneAsync(LoadSceneMode.Additive).Completed += ContinueSceneLoading;
+                }
+                else
+                {
+                    _isReady = true;
+                    if (_operation.Completed != null)
+                    {
+                        _operation.Completed.Invoke(SceneLoadingResult.Succeeded);
+                    }
+                }
+            }
+            else
+            {
+                if (_operation.Completed != null)
+                {
+                    _operation.Completed.Invoke(SceneLoadingResult.Failed);
+                }
+            }
+        }
+    }
+}
